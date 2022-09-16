b67fe202053c8428a5229bdbc549b165dbe6d1c8
Yaamto <bilou728@hotmail.fr>
2022-09-16 15:29:20 +0200

    Repost added in userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,67 +1,69 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema
-const bcrypt = require('bcrypt');
-
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const UserSchema = new Schema(
   {
-        username: {
-            type: String,
-            required: true,
-            unique: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        isAdmin: {
-            type: Boolean,
-            default: false,
-        },
-        profilePic: {
-            type: String,
-        },
-        bio: {
-          type: String
-        },
-        followers: {
-          type: [String]
-        },
-        following: {
-          type: [String]
-        },
-        likes: {
-          type: [String]
-        },
-        reposter: {
-          type: [String]
-        }
-        
-        },
-        {
-          timestamps: true,
-        })
-        UserSchema.pre("save", async function(next) {
-            const salt = await bcrypt.genSalt();
-            this.password = await bcrypt.hash(this.password, salt);
-            next();
-          });
-        
-          UserSchema.statics.login = async function(email, password) {
-            const user = await this.findOne({ email });
-            if (user) {
-              const auth = await bcrypt.compare(password, user.password);
-              if (auth) {
-                return user;
-              }
-              throw Error('incorrect password');
-            }
-            throw Error('incorrect email')
-          };
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    profilePic: {
+      type: String,
+    },
+    bio: {
+      type: String,
+    },
+    followers: {
+      type: [String],
+    },
+    following: {
+      type: [String],
+    },
+    likes: {
+      type: [String],
+    },
+    repost: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "post",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
-        module.exports = {User: mongoose.model('user', UserSchema )};
+UserSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error("incorrect password");
+  }
+  throw Error("incorrect email");
+};
+
+module.exports = { User: mongoose.model("user", UserSchema) };
